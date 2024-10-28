ce00fdebcf68e03074de5ce1218b8688f148e00a
devH <146154052+0umj@users.noreply.github.com>
2024-10-29 08:58:17 +0900

    feat: Jump 애니메이션 수정

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 
 public class PlayerAnimationController : AnimationController
 {
-    
     private int IsWalk = Animator.StringToHash("IsWalk");
     private int IsBackWalk = Animator.StringToHash("IsBackWalk");
+    private int Jumping = Animator.StringToHash("Jumping");
+    private int Landing = Animator.StringToHash("Landing");
 
     private PlayerController controller;
+    private bool isJumping = false;
 
     protected override void Awake()
     {
@@ -19,13 +22,17 @@ public class PlayerAnimationController : AnimationController
     private void Start()
     {
         controller.OnMoveEvent += Move;
+        controller.OnJumpEvent += Jump;
+        controller.OnLandEvent += Land;
+
     }
+
 
     private void Move(Vector2 dir)
     {
         float magnitude = dir.magnitude;
         bool isMovingForward = dir.y >= 0.0f; // 옆으로 가는 것도 전진으로 처리 
-        
+
         if (isMovingForward)
         {
             animator.SetBool(IsBackWalk, false);
@@ -37,13 +44,25 @@ public class PlayerAnimationController : AnimationController
             animator.SetBool(IsBackWalk, magnitude > 0.5f);
         }
     }
-    
+
+
+    private void Jump()
+    {
+        if(isJumping)
+            return;
+        
+        isJumping = true;
+        animator.SetTrigger(Jumping);
+    }
+
+    private void Land()
+    {
+        isJumping = false;
+        animator.SetTrigger(Landing);
+    }
     
     private void OnDestroy()
     {
         controller.OnMoveEvent -= Move;
     }
 }
-
-
-
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,28 +1,34 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
- 
+using Ray = UnityEngine.Ray;
+
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    
-    [Header("Movement Settings")] 
-    [SerializeField] private float moveSpeed;
+    [Header("Movement Settings")] [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField] private LayerMask groundLayerMask;
     [SerializeField] private float jumpPower;
     private Rigidbody rigidbody;
     private Vector2 curMovementDir;
 
-    [Header("Look Settings")] 
-    [SerializeField] private float minRotX;
+    [Header("Look Settings")] [SerializeField]
+    private float minRotX;
+
     [SerializeField] private float maxRotX;
     [SerializeField] private float lookSensitivity;
     private Vector2 curMouseDelta;
-    Camera camera = Camera.main;
-    
-    
-    public event Action<Vector2> OnMoveEvent;   
 
     
+    private const float LandingCheckDelay = 0.1f;
+
+    public event Action<Vector2> OnMoveEvent;
+    public event Action OnJumpEvent;
+    public event Action OnLandEvent;
+
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -33,10 +39,10 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    
+
     private void LateUpdate()
     {
-     }
+    }
 
     private void Move()
     {
@@ -59,6 +65,7 @@ public class PlayerController : MonoBehaviour
         {
             curMovementDir = Vector2.zero;
         }
+
         OnMoveEvent?.Invoke(curMovementDir);
     }
 
@@ -67,4 +74,70 @@ public class PlayerController : MonoBehaviour
         curMouseDelta = context.ReadValue<Vector2>();
     }
 
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && IsGrounded())
+        {
+            OnJumpEvent?.Invoke();
+        }
+    }
+    
+    private void PerformJump()
+    {
+        rigidbody.AddForce(Vector2.up * jumpPower,ForceMode.Impulse);
+        StartCoroutine(CheckLanding());
+    }
+
+    
+    private IEnumerator CheckLanding()
+    {
+        yield return new WaitForSeconds(LandingCheckDelay);
+        while (!IsGrounded())
+        {
+            yield return null;
+        }
+        OnLandEvent?.Invoke();
+    }
+
+
+    private bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down ),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            Gizmos.DrawRay(rays[i].origin, rays[i].direction * 0.1f);
+        }
+    }
 }
